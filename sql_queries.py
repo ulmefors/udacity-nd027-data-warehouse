03b6b90ba5c2dea395424961928a1db89276e74b
Marcus Ulmefors <marcus@ulmefors.com>
2020-04-04 14:11:01 +0200

    feat: Staging tables

--- sql_queries.py
+++ sql_queries.py
@@ -7,44 +7,114 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS stage_event"
+staging_songs_table_drop = "DROP TABLE IF EXISTS stage_song"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE IF NOT EXISTS stage_event
+    (
+        artist          TEXT,
+        auth            TEXT,
+        firstName       TEXT,
+        gender          TEXT,
+        itemInSession   INTEGER,
+        lastName        TEXT,
+        length          FLOAT4,
+        level           INTEGER,
+        location        TEXT,
+        method          TEXT,
+        page            TEXT,
+        registration    FLOAT8,
+        sessionId       INTEGER,
+        song            TEXT,
+        status          INTEGER,
+        ts              BIGINT,
+        userAgent       TEXT,
+        userId          TEXT
+    )
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE IF NOT EXISTS stage_song
+    (
+        song_id             TEXT,
+        title               TEXT,
+        duration            FLOAT4,
+        year                SMALLINT,
+        artist_id           TEXT,
+        artist_name         TEXT,
+        artist_latitude     REAL,
+        artist_longitude    REAL,
+        artist_location     TEXT,
+        num_songs           INTEGER
+    )
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplay
+    (
+
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS user
+    (
+
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS song
+    (
+
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artist
+    (
+
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time
+    (
+
+    )
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    COPY {} FROM {}
+    IAM_ROLE {}
+    JSON {};
+""").format(
+    'stage_event',
+    config['S3']['LOG_DATA'],
+    config['IAM_ROLE']['ARN'],
+    config['S3']['LOG_JSONPATH']
+)
 
 staging_songs_copy = ("""
-""").format()
+    COPY {} FROM {}
+    IAM_ROLE {}
+    JSON {};
+""").format(
+    'stage_song',
+    config['S3']['SONG_DATA'],
+    config['IAM_ROLE']['ARN'],
+    'AUTO'
+)
 
 # FINAL TABLES
 
